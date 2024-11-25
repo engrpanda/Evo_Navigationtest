08e10207ff70a8e4044c6d53c509ee5e2245dbdc
engrpanda <engrpandaece@gmail.com>
2024-11-25 16:53:47 +0800

    add map tools

--- app/src/main/java/com/evodesign/navtest/MainActivity2.java
+++ app/src/main/java/com/evodesign/navtest/MainActivity2.java
@@ -2,11 +2,11 @@ package com.evodesign.navtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.Toast;
 
 
 public class MainActivity2 extends AppCompatActivity {
@@ -16,14 +16,30 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
-        Button button = findViewById(R.id.start);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button navigation = findViewById(R.id.navigation);
+        navigation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start the second activity
                 Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button map = findViewById(R.id.maptools);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Try using an implicit intent to open the map tool app
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.ainirobot.maptool");
+
+                if (intent != null) {
+                    // Launch the map tool app
+                    startActivity(intent);
+                } else {
+                    // Handle the case where the app is not installed
+                    Toast.makeText(MainActivity2.this, "Map tool app not installed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
